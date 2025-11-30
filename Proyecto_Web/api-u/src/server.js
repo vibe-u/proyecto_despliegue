0c6b116261686506850b4720c28c53a222b3718d
Santy <santiago.vargas@epn.edu.ec>
2025-11-30 17:11:51 -0500

    Backend VIBE-U listo para producción

--- Proyecto_Web/api-u/src/server.js
+++ Proyecto_Web/api-u/src/server.js
@@ -1,3 +1,4 @@
+// INTENTOS DL DSPLIEGUE
 // server.js
 import express from "express";
 import cors from "cors";
@@ -9,25 +10,38 @@ import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
 
 const app = express();
-
+const allowedOrigins = [
+    process.env.URL_FRONTEND
+];
 // ✅ Middlewares
 app.use(cors({
-    origin: process.env.URL_FRONTEND // https://proyectovibe.netlify.app
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+        } else {
+        callback(new Error("No permitido por CORS"));
+        }
+    },
+    credentials: true
 }));
+
 app.use(express.json({ limit: "10mb" })); // aumento límite por si suben imágenes grandes
 
 // ✅ Configuración de Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 // Variables globales
 app.set("port", process.env.PORT || 3000);
 
 // Rutas
-app.get("/", (req, res) => res.send("Server on"));
+app.get("/", (req, res) => res.json({ 
+    msg: "VIBE-U API funcionando ✅",
+    version: "1.0.0"
+}));
 app.use("/api/usuarios", usuarioRouter);
 
 // Manejo de rutas no encontradas
